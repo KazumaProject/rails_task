e466445ec68ef8fc35e0a2316ee9a4913d236ef6
Kazu <59742125+KazumaProject@users.noreply.github.com>
2023-01-21 02:49:59 -0500

    edit and delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,24 @@ class TasksController < ApplicationController
         end
     end
 
+    def edit
+        @task = Task.find(params[:id])
+    end
+
+    def update
+        @task = Task.find(params[:id])
+        if @task.update(task_params)
+            redirect_to root_path
+        else
+            render 'edit', status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        redirect_to root_path, status: :see_other
+    end
     private 
     def task_params
         params.require(:task).permit(:title)
